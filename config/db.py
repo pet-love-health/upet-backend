c9c6babb80d790c55711e581f3ba319592207f52
toybonnie1672-cmd <toybonnie1672@gmail.com>
2025-11-10 13:08:04 -0500

    docs: Update db link.

--- config/db.py
+++ config/db.py
@@ -11,8 +11,7 @@ MYSQL_PORT = '3306'
 MYSQL_DATABASE = 'defaultdb'
 #mysqlsh --sql --host=sfo1.clusters.zeabur.com --port=30777 --user=root --password=Y9UEFrxH14OgZ872K6TueyJjD53mts0Q --schema=zeabur
 #URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
-URL_DATABASE = os.getenv('URL_DATABASE', "mysql+pymysql://root:PkkecnvYKOQhVcpALNQIamxkEdNzJcHG@interchange.proxy.rlwy.net:31764/upet")
-
+URL_DATABASE = os.getenv('URL_DATABASE', "mysql+pymysql://root:LQwSCrOEUrNyvKnHVegcMYfKevjOaPOE@switchback.proxy.rlwy.net:29642/upet")
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
