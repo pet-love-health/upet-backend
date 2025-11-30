75a61af6de61f9305f50f8a1ebf626567a2e9a18
MarceloRenteriaUPC <149633136+MarceloRenteriaUPC@users.noreply.github.com>
2025-11-30 03:54:52 -0500

    fix: Idk.

--- pets/domain/services/petService.py
+++ pets/domain/services/petService.py
@@ -51,8 +51,8 @@ class PetServices:
             battery = 100.0,
             location = LocationType(latitude=0.0, longitude=0.0)
         )
-        SmartCollarService.add_smart_collar(smartCollar)
-        SmartCollarService.change_pet_association(new_pet.id,new_pet.id)
+        SmartCollarService.add_smart_collar(smartCollar,db=db)
+        SmartCollarService.change_pet_association(new_pet.id,new_pet.id,db=db)
 
         medicalHistory = MedicalHistorySchemaPost(
             petId=new_pet.id,
